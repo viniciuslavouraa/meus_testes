8ffcb89a1b25e4db887c9f2aa496225ae3f118b1
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-15 17:03:08 -0300

    :-1:

--- CSS_training/GPT_training/gpt.js
+++
@@ -1,9 +0,0 @@
-const elemento = document.getElementById("meu-elemento");
-const botao = document.getElementById("botao");
-
-botao.addEventListener("click", function() {
-  elemento.style.backgroundColor = "blue";
-});
-
-
-
