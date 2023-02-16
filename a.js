c629fdeed145e2e8c63a8bdd20830596def0ff18
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-02-16 17:14:30 -0300

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,12 @@
+const numbers = [0]
+var maxnumber = numbers.length
+console.log (`o valor inicial é ${maxnumber}`)
+if (maxnumber < 4) {
+    while (maxnumber < 4) {
+        maxnumber++
+        console.log (maxnumber)
+    }
+} else {
+    console.log (`${numbers} é o maximo de numeros`)
+}
 
