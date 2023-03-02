a49262dff5b52ebadb67c26e645e39d366f016f9
Vinicius Lavoura <121114895+viniciuslavouraa@users.noreply.github.com>
2023-03-02 20:48:06 -0300

    terminado primeira parte da landing page

---
+++ CSS_training/landingpage01/menu.js
@@ -0,0 +1,51 @@
+let btn = document.querySelector('#btn')
+let menu_open = document.querySelector('#content-menu')
+
+btn.addEventListener('click', function() {
+    if (menu_open.style.display == 'none') {
+        menu_open.style.display = 'flex'
+        menu_open.style.position = 'absolute'
+        btn.style.transform = 'rotate(90deg)'
+        btn.style.transition = '600ms'
+    } else {
+        menu_open.style.display = 'none'
+        btn.style.transform = ''
+    }
+})
+
+let btn1 = document.querySelector('#btn1')
+let btn2 = document.querySelector('#btn2')
+let btn3 = document.querySelector('#btn3')
+
+btn1.addEventListener('click',mudar), btn2.addEventListener('click',mudar2), btn3.addEventListener('click',mudar3)
+
+function mudar() {
+    let content = document.querySelectorAll('.content')[0];
+    if (content.style.top == '0' || content.style.top == '') {
+        content.style.top = '-100px';
+    } else {
+        content.style.top = '0';
+    }
+}
+
+function mudar2() {
+     let content = document.querySelectorAll('.content')[1]
+     if (content.style.top == '0' || content.style.top == '') {
+        content.style.top = '-100px'
+     } else {
+        content.style  = 0
+     }
+}
+
+function mudar3() {
+    let content = document.querySelectorAll('.content')[2]
+    if (content.style.to == '0' || content.style.top == '') {
+        content.style.top = '-100px'
+    } else {
+        content.style.top = '0'
+    }
+}
+
+
+
+
